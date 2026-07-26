d6fe959607b5ad24289dac3301e167a186440f18
Prasad <prasadbhakare.5@gmail.com>
2026-07-26 18:11:42 +0530

    LVTI var keyword

---
+++ _9LocalVariableTypeInference/Demo.java
@@ -0,0 +1,14 @@
+package _9LocalVariableTypeInference;
+
+public class Demo {
+    public static void main(String[] args) {
+        var a = "Prasad";
+        String var = "Name";
+        var arr = new int[10];
+        arr = new int[] {10, 33};
+        var obj = new MyClass();
+    }
+}
+class MyClass{
+
+}
